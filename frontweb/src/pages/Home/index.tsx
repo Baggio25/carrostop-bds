8179fc8f481ab9fe2de8e857c3f0e6daa9a086b6
Rodrigo Baggio <rodrigo.baggio.si@gmail.com>
2021-06-30 17:22:54 -0300

    Layout x-small initiated

--- frontweb/src/pages/Home/index.tsx
+++ frontweb/src/pages/Home/index.tsx
@@ -10,13 +10,15 @@ const Home = () => {
         <div className="home-card-top">
           <div className="home-content-container-card-top">
             <h1>O carro perfeito para você</h1>
+            <p>Conheça nossos carros e dê mais um passo na realização do seu sonho</p>
           </div>
           <div className="home-image-container-card-top">
             <MainImage />
           </div>
         </div>
         <div className="home-card-bottom">
-          <div className="home-container-card-bottom"></div>
+          <div className="home-container-card-bottom">
+          </div>
         </div>
       </div>
     </>
