5d18001db2873ecdb6ef02c19d8bc840cc178e31
Rodrigo Baggio <rodrigo.baggio.si@gmail.com>
2021-06-30 15:50:38 -0300

    Navbar styling

--- frontweb/src/App.tsx
+++ frontweb/src/App.tsx
@@ -1,10 +1,13 @@
-import React from 'react';
 import './assets/styles/custom.scss';
 import './App.css';
+import Navbar from './components/Navbar';
 
 function App() {
   return (
-    <h1>CarrosTop</h1>
+    <>
+      <Navbar />
+      <h1>Carros Top</h1>
+    </>
   );
 }
 
